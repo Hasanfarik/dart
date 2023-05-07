1e46cb9dcdfcdb72e7a2a1abeb81cb851c6ad0a0
Hasanfarik <130719464+Hasanfarik@users.noreply.github.com>
2023-05-07 20:58:22 +0300

    Salary total

---
+++ bin/homeworke.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+void salarytotal(){
+  print('Hasan farik');
+  var salary =1000;
+  var bouns=500;
+  var salarytotal = salary+bouns;
+  print(salarytotal );
+}  
+salarytotal();
+}
