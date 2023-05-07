b1e03f727e112865f5771ab6f145a6823bbf8616
Hasanfarik <130719464+Hasanfarik@users.noreply.github.com>
2023-05-07 13:46:24 +0300

    methods in dart

---
+++ bin/new.dart
@@ -0,0 +1,12 @@
+void main(){
+//methods in dart 
+//String name(String A, String B){
+//String A='Hasan';
+//String B='fadeel';
+//return A+B;
+//}
+//name();
+//print(name('Hasan','fadeel'));
+print('*'*20);
+
+}
