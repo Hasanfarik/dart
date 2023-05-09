ce8693bdaaf5f235c68ca3301ad570ef1ec84816
Hasanfarik <130719464+Hasanfarik@users.noreply.github.com>
2023-05-09 12:08:21 +0300

    method.dart

--- bin/homeworke.dart
+++ bin/homeworke.dart
@@ -1,10 +1,18 @@
 void main(List<String> args) {
-void salarytotal(){
-  print('Hasan farik');
-  var salary =1000;
-  var bouns=500;
-  var salarytotal = salary+bouns;
-  print(salarytotal );
-}  
-salarytotal();
+// void salarytotal(){
+//   print('Hasan farik');
+//   var salary =1000;
+//   var bouns=500;
+//   var salarytotal = salary+bouns;
+//   print(salarytotal );
+// }  
+// salarytotal();
+void functionary (String name ,var salary,var bouns){
+  dynamic salarytotal= salary +bouns;
+  return print(name);
+}
+
+functionary('Hasan farik' ,1000 , 500);
+// functionary(salarytotal);
+
 }
