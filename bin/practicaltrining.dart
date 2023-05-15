4c1fc58e0fa57ee1a7def98c1cddfee20cf46e3a
Hasanfarik <130719464+Hasanfarik@users.noreply.github.com>
2023-05-15 22:43:09 +0300

    practicaltrining in dart

---
+++ bin/practicaltrining.dart
@@ -0,0 +1,61 @@
+//void main() is the starting point where the execution of your program begins.
+void main()
+//void main() is the starting point where the execution of your program begins.
+{
+  print('Hello word'); //Each code statement must end with a semicolon.
+var name = 'Hasan';
+print(name);
+//Basic Dart Program To Join One Or More Variables 
+var firstname = 'hasan';
+var lastname = 'farik';
+print('Full name is $firstname $lastname');
+print('*'*40);
+int num1 = 10;
+int num2 = 3;
+int sum = num1 + num2;
+int Subtract = num1 - num2;
+int mul = num1 * num2 ;
+double div = num1 / num2; // It is double because it outputs number with decimal
+print('the sum is $sum');
+print('the Subtract is $Subtract');
+print('the mul is $mul');
+print('the div is $div'); 
+print('*'*40);
+const z=3;
+ //z = 4; Constant variables can't be assigned a value.
+print("value of z is $z");
+print ('*'*40);
+int a = 100;
+double b = 130.2; 
+num c = 50;
+num d = 50.4;
+num sum1 = a+ b+ c+ d;
+print ('$a');
+print(b);
+print(c);
+print(d);
+print ('$sum1');
+print('*'*40);
+double oob = 130.645823;
+print( oob.toStringAsFixed(3));
+print('*'*40);
+String name1='''
+hasan
+fadeel
+farkh
+'''
+;
+print ( name1);
+print('*'*40);
+//Special Character
+print ( "I am from \n JOR");
+print ( "I am from \t JOR");
+print('*'*40);
+num prize = 10;
+String withoutrawstring ="the value of prize is\t $prize";
+String withrawstring =r"the value of prize is /t $prize";
+print("without Raw : $withoutrawstring");
+print("with Raw : $withrawstring");
+print('*'*40);
+
+}
