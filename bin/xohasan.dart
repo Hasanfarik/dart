b8a1a071cc4d0b028b82a15339c0565a5faeea6f
Hasanfarik <130719464+Hasanfarik@users.noreply.github.com>
2023-05-21 01:22:01 +0300

    xohasan in dart

--- bin/xohasan.dart
+++ bin/xohasan.dart
@@ -1,10 +1,69 @@
+import 'dart:io';
+import 'dart:svg';
+bool winner = false;
+bool isxturn = true;
+int  movecount = 0;
 List<String> values = ['1','2','3','4','5','6','7','8','9'];
-
+List<String> combinations=['012','048','147','246', '258','345','678'];
 
 void main() {
   board();
+  get_char();
 }
 
+
+bool chek_combination(String combination, String check){
+  List<int> numbers =combination.split('').map((item){
+    return int.parse(item);
+  }).toList();
+  bool match = false;
+  for (final item in numbers){
+    if (values[item]== check){
+      match = true;
+    }
+    else{
+      match = false;
+      break;
+    }
+  }
+  return match;
+}
+
+
+void check_winner (player){
+  for ( final item in combinations){
+    bool combination_validity = chek_combination(item, player);
+    if (combination_validity== true){
+      print('$player won....');
+      winner = true;
+      break;
+    }
+  }
+}
+
+
+void get_char(){
+  print("choose number for ${isxturn==true ? 'x' : 'o' }");//اذا كان العيب  x true print X  والعكس
+  int number = int.parse(stdin.readLineSync()!);
+  values[number -1] = isxturn? 'x':'o';
+  isxturn= !isxturn;
+  movecount++;
+
+  if (movecount == 9){
+    winner= true;
+    print('DRW....');
+  }
+  else {
+    board();
+  }
+
+  check_winner('x');
+  check_winner('o');
+
+  if(winner == false){
+    get_char();
+  }
+}
 
 void board(){
   print('  |   |   |');
