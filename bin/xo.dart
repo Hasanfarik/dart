70b370d3e96ede1b378018e0a653711660f1050d
Hasanfarik <130719464+Hasanfarik@users.noreply.github.com>
2023-05-21 00:03:20 +0300

    XO

--- bin/xo.dart
+++ bin/xo.dart
@@ -1,12 +1,92 @@
-void main(List<String> args) {
- 
-List <String>Game =['1','2','3','4','5','6','7','8','9'];
+import 'dart:io';
 
-  print(' __ ___ ___');
-  print('|${Game[0]} | ${Game[1]} |  ${Game[2]}|');
-  print('|__|___|___|');
-  print('|${Game[3]} | ${Game[4]} |  ${Game[5]}|');
-  print('|__|___|___|');
-  print('|${Game[6]} | ${Game[7]} |  ${Game[8]}|');
-  print('|__|___|___|');
+void main(){
+TicTacToe game = TicTacToe();
+
+while (true) {
+  game.displayBoard();
+  stdout.write('Enter a number (1-9) for ${game.currentplayer}');
+  int? position = int.tryParse(stdin.readLineSync() ?? '');
+  
+  if (position != null && position >= 1 && position <=9){
+    game.makemove(position);
+  }
+  if (game.checkwinner()){
+    game.displayBoard();
+    print('${game.currentplayer} wins!');
+    break;
+  }
+  bool isFull = true;
+  for (int i=0; i < 9; i++){
+    if (game.board[i] == ' '){
+      isFull = false;
+      break;
+    }
+  }
+  if (isFull) {
+    game.displayBoard();
+    print('Tie game!');
+    break;
+    
+  }
+}
+
+}
+class TicTacToe {
+  List<String>board;
+  String currentplayer;
+
+  TicTacToe()
+  : board = List.generate(9, (_) => ' '),
+    currentplayer = 'x';
+
+  void makemove (int position){
+    if (board[position -1]==' '){
+      board[position-1]= currentplayer;
+      currentplayer = currentplayer =='x'? 'o' : 'x';
+
+    }
+
+  } 
+  bool checkwinner(){
+    for (int i=0; i<9; i +=3){
+      if (board[i] !=' ' && board[i] == board[ i+1] && board[i+1]== board[i+2]){
+        return true;
+      }
+    }
+    for (var i = 0; i < 3; i++) {
+      if (board[i] !=' ' && board[i]== board[i+1]&&board[i+1]==board[i+2]) {
+        return true;
+        
+      }
+    }
+    for (var i = 0; i < 3; i++) 
+    {
+    if (board[i] !=' ' &&board[i] == board[i+3]&&board[i+3]==board[i+6]) {
+      return true;
+      
+    }  
+    }
+    // ignore: unrelated_type_equality_checks
+    if (board[0] !=' ' && board[0]== board[4] &&board[4]==board[8]) {
+      return true;
+      
+    }
+    if (board[2] !=' ' && board[2] == board[4]&&board[4]==board[6]){
+      return true;
+
+    }
+    return false;
+  }
+ void displayBoard(){
+  print(' ---+----+---');
+  print('|${board[0]}  | ${board[1]}  | ${board[2]} |');
+  print('|---+----+---|');
+  print('|${board[3]}  | ${board[4]}  | ${board[5]} |');
+  print('|---+----+---|');
+  print('|${board[6]}  |  ${board[7]} | ${board[8]} |');
+  print(' ---+----+---');
+  print('');
+  print('Current player : $currentplayer');
+ } 
 }
